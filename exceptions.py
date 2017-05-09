c3e9179fdf13aa1ba93c1d861f95f1b8312dfc00
phi-line <phi.liney@gmail.com>
2017-05-08 19:33:55 -0700

    added doctrings to all major helper functions

--- exceptions.py
+++ exceptions.py
@@ -4,7 +4,13 @@ from globals import Globals as g
 class Rotom():
 
     @staticmethod
-    def rotom_embed(title='', desc=''):
+    def rotom_embed(title : str, desc : str):
+        '''
+        This is a helper method to display a rotom exception embed to the chat
+        :param title: str             - The title for the embed
+        :param desc: str              - The desc for the embed
+        :return: embed: discord.Embed - The embed object
+        '''
         embed = discord.Embed(title=title, description=desc, color=g.COLOR)
         embed.set_thumbnail(url=g.DEX_URL)
         return embed
